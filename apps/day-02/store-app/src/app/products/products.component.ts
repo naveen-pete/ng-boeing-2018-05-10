c2d55f6e22c0b44564cc43e3d911f4000a8eddab
Naveen Pete <naveen.shenoy.blr@gmail.com>
2018-05-11 09:18:35 +0530

    add start projects for day 2

---
+++ apps/day-02/store-app/src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html'
+})
+export class ProductsComponent {
+  product: Product;
+
+  constructor() {
+    this.product = {
+      id: 1,
+      name: 'Honor 9 Lite',
+      description: 'Smart phone',
+      price: 20000,
+      isAvailable: true
+    };
+  }
+}
